01912f4e0b7ad0c5c5482ed98ce6fd49ad56d572
d.romanenko <dimonishe@gmail.com>
2020-06-17 08:20:25 +0300

    Second create

--- buh_report.py
+++ buh_report.py
@@ -1,8 +1,18 @@
 import bitrix
 
 # Получим список закрытых заказов
+sql = '''
+select 
+	*
+from 
+	oc_order_starta
+where
+	LAST_STATE = 1 and
+    STATUS_ORDER = 3 and
+    ORDER_ID > 122
+    '''
 
-a = bitrix.Order(207)
-for i in a.checks:
-    print(i.type,
-          i.total)
+bitrix.cursor.execute(sql)
+res = bitrix.cursor.fetchall()
+for i in res:
+    print()
