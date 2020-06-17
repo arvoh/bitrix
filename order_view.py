0c808a72814508f97c8db12a9d3e49c25116cc61
d.romanenko <dimonishe@gmail.com>
2020-06-17 11:10:58 +0300

    Second create

--- order_view.py
+++ order_view.py
@@ -4,7 +4,11 @@ while True:
     order_id = int(input('Номер заказа: '))
     if order_id == 0:
         exit()
-    a = bitrix.Order(order_id)
+    try:
+        a = bitrix.Order(order_id)
+    except Exception as e:
+        print(e)
+        continue
     print('Заказ № ',order_id)
     print('Оплата ', a.total)
     print('Сумма по банку ', a.payment.finish_sum)
