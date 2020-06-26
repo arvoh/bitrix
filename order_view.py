0b1d235908d95ec52b829f3943de5d8c244f9005
d.romanenko <dimonishe@gmail.com>
2020-06-26 15:47:05 +0300

    Добален параметр good - отключающий проверку суммы

--- order_view.py
+++ order_view.py
@@ -9,10 +9,11 @@ while True:
     except Exception as e:
         print('Ошибка\n', e)
         continue
-    print('Заказ № ',order_id)
+    print('Заказ № ', order_id)
+    print('Статус Битрикс', a.bitrix_status)
     print('Сумма из ДиФ ', a.total)
     print('Сумма по завершения ', a.payment.finish_sum)
-    print('Сумма по холдирования ', a.payment.finish_sum)
+    print('Сумма по холдирования ', a.payment.holded_sum)
     print('Разблокировка у клиента ', a.payment.delta)
     print('Возврат по банку', a.payment.refunded_amount)
     print('Статус по банку ', a.payment.staus)
